78465a687b29ea86c15c45a04a4de1647c21983b
Herman Chan <hermchan89@gmail.com>
2017-06-26 19:03:17 -0700

    render twitch user data

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var accounts = ['freecodecamp', 'khuey', 'ogamingsc2', 'storbeck', 'terakilobyte', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff', 'MedryBW', 'brunofin', 'comster404', 'quill18'];
+var logo = 'https://i.imgur.com/vPEp5RQ.png';
+var streams = 'https://api.twitch.tv/kraken/streams/';
+var channels = 'https://wind-bow.glitch.me/twitch-api/channels/';
+var users = 'https://wind-bow.glitch.me/twitch-api/users/';
+
+
+function Account(name, logo){
+  this.name = name;
+  this.logo = logo;
+}
+
+$(document).ready(function() {
+  accounts.forEach(getUserInfo);
+});
+
+function getUserInfo(currentName) {
+  $.getJSON(users + currentName + '?callback=?', function(data) {
+    console.log(data);
+    if (data.logo === null) {
+      currentName = new Account(data.display_name, logo);
+      console.log('testing', currentName);
+    } else {
+      currentName = new Account(data.display_name, data.logo);
+    }
+    $("#streamList").append("<div><li><img src=" + data.logo + "></img>"+currentName.name+"</li></div>");
+  })
+  return currentName;
+}
